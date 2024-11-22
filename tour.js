aa32cf4136015c40cc755f527217a9759f4560e9
Gal Nadrag <gnadrag@gmail.com>
2024-11-22 07:13:27 +0100

    tour changes

--- tour.js
+++ tour.js
@@ -1,4 +1,19 @@
-const tg = new tourguide.TourGuideClient()
+const steps = [{
+  title: "Welcome to Our Tour!",
+  content: "We are excited to show you the key features of our website. Click 'Next' to start the tour.",
+  order: 0,
+  // No target for this step, as it's a title screen
+}, {
+  title: "Target",
+  content: "This is your target area",
+  target: "#target",
+  order: 1,
+}, 
+];
+
+const tg = new tourguide.TourGuideClient({
+  steps: steps,
+});
 
 function startTour() {
 	tg.start()
