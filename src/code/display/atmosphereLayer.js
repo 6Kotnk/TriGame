b0112b9e5fb80a18105e876b4cdafa2fc2203458
6Kotnk <gnadrag@gmail.com>
2025-06-05 21:28:41 +0200

    Adjust atm color

--- src/code/display/atmosphereLayer.js
+++ src/code/display/atmosphereLayer.js
@@ -96,7 +96,7 @@ class AtmosphereLayer {
         // This factor is to create the effect of a realistic thickening of the atmosphere coloring
         float factor = pow(dotP, atmPowFactor) * atmMultiplier;
         // Adding in a bit of dotP to the color to make it whiter while thickening
-        vec3 atmColor = vec3(0.25 + dotP/4.5, 0.25 + dotP/4.5, 1.0);
+        vec3 atmColor = vec3(0.35 + dotP/4.5, 0.35 + dotP/4.5, 1.0);
         // use atmOpacity to control the overall intensity of the atmospheric color
         gl_FragColor = vec4(atmColor, atmOpacity) * factor;
       }
