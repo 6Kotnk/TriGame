5cd0b1cea63c7b6584a54a56f1285102472b872f
6Kotnk <gnadrag@gmail.com>
2026-02-21 22:44:29 +0100

    read from view

--- src/code/database/database.js
+++ src/code/database/database.js
@@ -3,7 +3,7 @@ export class Database {
   constructor() {
     // These will need to be replaced with your actual Supabase project values
 
-    this.supabaseReadUrl = 'https://clmquoqxsfesbjpseuhj.supabase.co/rest/v1'; 
+    this.supabaseReadUrl = 'https://clmquoqxsfesbjpseuhj.supabase.co/rest/v1/scores_view'; 
     this.supabaseWriteUrl = 'https://clmquoqxsfesbjpseuhj.supabase.co/functions/v1/submit-score';
     this.supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsbXF1b3F4c2Zlc2JqcHNldWhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwNjQ3NjMsImV4cCI6MjA3MTY0MDc2M30.PnByNeob1w9h2lLWcuOoKR0k7wRtxJj-w8R0fjuKe_s'; // Your anon/public key
   }
@@ -40,7 +40,7 @@ export class Database {
   // Get all scores from database
   async getAllScores() {
     try {
-      const response = await fetch(`${this.supabaseReadUrl}/leaderboard_public?select=*`, {
+      const response = await fetch(`${this.supabaseReadUrl}?select=*`, {
         headers: {
           'apikey': this.supabaseKey,
           'Authorization': `Bearer ${this.supabaseKey}`,
@@ -61,7 +61,7 @@ export class Database {
   // Get top scores for leaderboard
   async getTopScores(limit = 10) {
     try {
-      const response = await fetch(`${this.supabaseReadUrl}/leaderboard_public?select=*&order=score.desc&limit=${limit}`, {
+      const response = await fetch(`${this.supabaseReadUrl}?select=*&order=score.desc&limit=${limit}`, {
         headers: {
           'apikey': this.supabaseKey,
           'Authorization': `Bearer ${this.supabaseKey}`,
